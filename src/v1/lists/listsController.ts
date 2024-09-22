d3969ccf40f2b5c9aa4e2da5f199f377cb688901
Steve Quince <squince@glgroup.com>
2024-09-22 16:29:54 -0400

    list route is fleshed out

--- src/v1/lists/listsController.ts
+++ src/v1/lists/listsController.ts
@@ -2,6 +2,8 @@
 import { Body, Controller, Delete, Get, Header, Path, Post, Put, Route, SuccessResponse, Tags} from "tsoa";
 
 import { List, ListCreationParams } from "./list";
+import { Category, CategoryCreationParams } from "../categories/category";
+import { Item } from "../items/item";
 import { ListsService } from "./listsService";
 import { ErrorCode } from "../../shared/errorHandler";
 
@@ -10,18 +12,115 @@ import { ErrorCode } from "../../shared/errorHandler";
 export class ListsController extends Controller {
   /**
    * @summary Creates a new list of items
-   * @param email - the email address of the user
+   * @param email the email address of the user
    * @returns The ID of the created list
    */
   @Post()
   @SuccessResponse(201, "Created")
-  public async create(@Header("X-Auth-User") email: string, @Body() newList: ListCreationParams ): Promise<string> {
+  public async create(@Header("X-Auth-User") email: string, @Body() newList: ListCreationParams ): Promise<Pick<List, "id">> {
     try {
-      const listId = await ListsService.create(newList);
-      return listId;
+      return await ListsService.create(newList);
     } catch (err: any) {
       err.code = ErrorCode.DATABASE_ERR
       throw err;
     }
+  };
+
+  /**
+   * @summary Adds a category to a list
+   * @param listId the ID of the list
+   * @param category the category to add
+   * @returns The ID of the created category
+   */
+  @Post("{listId}/categories")
+  @SuccessResponse(201, "Created")
+  public async addCategory(@Path() listId: string, @Body() category: CategoryCreationParams): Promise<Pick<Category, "id">> {
+    return await ListsService.addCategory(listId, category);
+  };
+
+  /**
+   * @summary Adds an item to a list
+   * @param listId the ID of the list
+   * @param item the item to add
+   * @returns The ID of the created item
+   */
+  @Post("{listId}/items")
+  @SuccessResponse(201, "Created")
+  public async addItem(@Path() listId: string, @Body() item: Item): Promise<Pick<Item, "id">> {
+    return await ListsService.addItem(listId, item);
+  };
+
+  /**
+   * @summary Purchases an item on a list
+   * @param listId the ID of the list
+   * @param itemId the ID of the item to purchase
+   * @returns boolean indicating the success of the operation
+   */
+  @Post("{listId}/items/{itemId}/purchase")
+  @SuccessResponse(201, "Created")
+  public async purchaseItem(@Path() listId: string, @Path() itemId: string): Promise<void> {
+    await ListsService.purchaseItem(listId, itemId);
+    return;
+  };
+
+  /**
+   * @summary Updates a list
+   * @param listId the ID of the list
+   * @returns A boolean indicating the success of the operation
+   */
+  @Put("{listId}")
+  @SuccessResponse(205, "Content Updated")
+  public async update(@Path() listId: string, @Body() updatedList: List): Promise<void> {
+    await ListsService.update(listId, updatedList);
+    return;
+  };
+
+  /**
+   * @summary Deletes a list of items
+   * @param listId the ID of the list
+   * @returns A boolean indicating the success of the operation
+   */
+  @Delete("{listId}")
+  @SuccessResponse(205, "Content Updated")
+  public async delete(@Path() listId: string): Promise<void> {
+    await ListsService.delete(listId);
+    return;
+  };
+
+  /**
+   * @summary Removes a category from a list
+   * @param listId the ID of the list
+   * @param categoryId the ID of the category to remove
+   * @returns The list of items
+   */
+  @Delete("{listId}/categories/{categoryId}")
+  @SuccessResponse(205, "Content Updated")
+  public async removeCategory(@Path() listId: string, @Path() categoryId: string): Promise<void> {
+    await ListsService.removeCategory(listId, categoryId);
+    return;
+  };
+
+  /**
+   * @summary Removes an item from a list
+   * @param listId the ID of the list
+   * @param itemId the ID of the item to remove
+   * @returns The list of items
+   */
+  @Delete("{listId}/items/{itemId}")
+  @SuccessResponse(205, "Content Updated")
+  public async removeItem(@Path() listId: string, @Path() itemId: string): Promise<void> {
+    await ListsService.removeItem(listId, itemId);
+    return;
+  };
+
+  /**
+   * @summary Retrieves the categories for a list
+   * @param listId the ID of the list
+   * @returns The list of categories
+   */
+  @Get("{listId}/categories")
+  @SuccessResponse(200, "OK")
+  public async getCategories(@Path() listId: string): Promise<Category[]> {
+    return await ListsService.getCategories(listId);
   };
 };
--- src/v1/lists/listsService.ts
+++ src/v1/lists/listsService.ts
@@ -1,17 +1,73 @@
 import path from "path";
 import { dbPost, extractDbResult } from "../../shared/dbDriver";
-import { ListCreationParams } from "./list";
+import { List, ListCreationParams } from "./list";
+import { Category, CategoryCreationParams } from "../categories/category";
+import { Item } from "../items/item";
 import { Logger, logger } from "../../shared/logger";
 
 const log: Logger = logger('List Service')
 
 export abstract class ListsService {
-  public static async create(list: ListCreationParams): Promise<string> {
+  public static async create(list: ListCreationParams): Promise<Pick<List, "id">> {
     const { name, ownerId, groupId } = list;
     const createTemplate = path.join(__dirname, '.sql/createList.sql');
     const [rows, fields] = await dbPost(createTemplate, { name, ownerId, groupId });
     const results = extractDbResult(rows);
     const listId = results[0].id;
-    return listId;
+    return { id: listId };
+  };
+
+  public static async update(listId: string, list: ListCreationParams): Promise<void> {
+    const { name, ownerId, groupId } = list;
+    const updateTemplate = path.join(__dirname, '.sql/updateList.sql');
+    await dbPost(updateTemplate, { id: listId, name, ownerId, groupId });
+    return;
+  };
+
+  public static async delete(listId: string): Promise<void> {
+    const deleteTemplate = path.join(__dirname, '.sql/deleteList.sql');
+    await dbPost(deleteTemplate, { id: listId });
+    return;
+  };
+
+  public static async addCategory(listId: string, category: CategoryCreationParams): Promise<Pick<Category, "id">> {
+    const addCategoryTemplate = path.join(__dirname, '.sql/addCategory.sql');
+    const [rows, fields] = await dbPost(addCategoryTemplate, { id: listId, category });
+    const results = extractDbResult(rows);
+    const categoryId = results[0].id;
+    return { id: categoryId };
+  };
+
+  public static async getCategories(listId: string): Promise<Array<Category>> {
+    const getCategoriesTemplate = path.join(__dirname, '.sql/getCategories.sql');
+    const [rows, fields] = await dbPost(getCategoriesTemplate, { id: listId });
+    const results = extractDbResult(rows);
+    return results;
+  };
+
+  public static async removeCategory(listId: string, categoryId: string): Promise<void> {
+    const removeCategoryTemplate = path.join(__dirname, '.sql/removeCategory.sql');
+    await dbPost(removeCategoryTemplate, { id: listId, categoryId });
+    return;
+  };
+
+  public static async addItem(listId: string, item: Item): Promise<Pick<Item, "id">> {
+    const addItemTemplate = path.join(__dirname, '.sql/addItem.sql');
+    const [rows, fields] = await dbPost(addItemTemplate, { id: listId, item });
+    const results = extractDbResult(rows);
+    const itemId = results[0].id;
+    return { id: itemId };
+  };
+
+  public static async removeItem(listId: string, itemId: string): Promise<void> {
+    const removeItemTemplate = path.join(__dirname, '.sql/removeItem.sql');
+    await dbPost(removeItemTemplate, { id: listId, itemId });
+    return;
+  };
+
+  public static async purchaseItem(listId: string, itemId: string): Promise<void> {
+    const purchaseItemTemplate = path.join(__dirname, '.sql/purchaseItem.sql');
+    await dbPost(purchaseItemTemplate, { id: listId, itemId });
+    return;
   };
 };
