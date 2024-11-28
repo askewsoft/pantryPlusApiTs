92e334ad8e8bc79bcf551ecb9722adea7cab41a7
Steve Quince <squince@glgroup.com>
2024-11-28 17:42:15 -0500

    fixing getLists sql to align with app expectations

--- src/v1/shoppers/sql/getLists.sql
+++ src/v1/shoppers/sql/getLists.sql
@@ -1,25 +1,30 @@
 -- gets all the lists for the shopper ID
 -- associated with the user
 
-SET @shopperId = :shopperId;
+SET @shopperIdTxt = :shopperId;
 
 WITH shopperGroups as (
-  SELECT gr.ID as GROUP_ID, gr
+  SELECT gr.ID as GROUP_ID
   FROM PANTRY_PLUS.GROUP_SHOPPER_RELATION gsr
   JOIN PANTRY_PLUS.SHOPPER sh
     ON sh.ID = gsr.SHOPPER_ID
-    AND sh.ID_TXT = @shopperId
-  JOIN PANTRY_PLUS.GROUP gr ON gr.ID = gsr.GROUP_ID
+    AND sh.ID = uuid_to_bin(@shopperIdTxt)
+  JOIN PANTRY_PLUS.GROUP gr
+    ON gr.ID = gsr.GROUP_ID
+    AND gr.OWNER_ID <> uuid_to_bin(@shopperIdTxt)
 )
 
 SELECT
-  ls.ID_TXT as ID,
+  bin_to_uuid(ls.ID) as ID,
   ls.NAME,
-  CASE
-    WHEN sh.ID_TXT = @shopperId THEN TRUE
-    ELSE FALSE
-  END AS IS_LIST_OWNER
+  bin_to_uuid(ls.OWNER_ID) as OWNER_ID
 FROM shopperGroups sg
 JOIN PANTRY_PLUS.LIST ls ON ls.GROUP_ID = sg.GROUP_ID
-LEFT JOIN PANTRY_PLUS.SHOPPER sh ON sh.ID = ls.OWNER_ID
+UNION
+SELECT
+  bin_to_uuid(ID) as ID,
+  NAME,
+  bin_to_uuid(OWNER_ID) as OWNER_ID
+FROM PANTRY_PLUS.LIST
+WHERE OWNER_ID = uuid_to_bin(@shopperIdTxt)
 ;
