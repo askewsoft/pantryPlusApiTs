370b70fc35652334cc66bef1146d4cf5973568ca
Steve Quince <squince@glgroup.com>
2025-08-16 17:39:28 -0400

    nickname must not be empty string or whitespace-only

--- src/v2/shoppers/shopper.ts
+++ src/v2/shoppers/shopper.ts
@@ -6,7 +6,10 @@ export interface Shopper {
      * @pattern ^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$ Invalid UUID format
      */
     id: string;
-    /** The nickname of the shopper */
+    /** The nickname of the shopper
+     * @minLength 1 Must not be empty
+     * @pattern ^\S+.*\S+$ Must not be only whitespace
+     */
     nickname: string;
     /** The email of the shopper
      * @pattern ^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$ Invalid email address
