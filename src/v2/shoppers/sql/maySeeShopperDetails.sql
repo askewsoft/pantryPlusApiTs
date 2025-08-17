b476381f89342777bc41cf049719f921d37e8e02
Steve Quince <squince@glgroup.com>
2025-08-17 16:15:34 -0400

    fixing bug in which an owner of a cohort could not add an item to a category of a list shared with that cohort

--- src/v2/shoppers/sql/maySeeShopperDetails.sql
+++ src/v2/shoppers/sql/maySeeShopperDetails.sql
@@ -1,5 +1,6 @@
 -- confirm access to see shopper details
 -- a user can see the details of shoppers in a cohort shared with the user
+-- or if the user owns the cohort
 
 SET @userEmail = :email;
 SET @shopperId = UUID_TO_BIN(:id);
@@ -12,15 +13,22 @@ SELECT EMAIL INTO @shopperEmail FROM PANTRY_PLUS.SHOPPER WHERE ID = @shopperId
 SELECT IF (
     STRCMP(@shopperEmail, @userEmail) = 0, 1, (SELECT IF (
         EXISTS (
-            SELECT csr.COHORT_ID
+            SELECT 1 AS ALLOWED
             FROM PANTRY_PLUS.COHORT_SHOPPER_RELATION csr
             WHERE csr.SHOPPER_ID = @shopperId
                 AND EXISTS (
-                    SELECT u.COHORT_ID
+                    SELECT 1
                     FROM PANTRY_PLUS.COHORT_SHOPPER_RELATION u
                     WHERE u.SHOPPER_ID = @userId
                         AND u.COHORT_ID = csr.COHORT_ID
                 )
+            UNION
+            SELECT 1 AS ALLOWED
+            FROM PANTRY_PLUS.COHORT_SHOPPER_RELATION csr
+            JOIN PANTRY_PLUS.COHORT c ON c.ID = csr.COHORT_ID
+            WHERE csr.SHOPPER_ID = @shopperId
+                AND c.OWNER_ID = @userId
         ), 1, 0))
 ) AS ALLOWED
+;
 ;
